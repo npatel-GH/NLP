c6669edbb724b768bde47304e3d99d0a30368447
Neha Patel - GH <npatel@guidehouse.com>
2021-11-17 12:29:04 -0600

    Learning_Complete_-Ex

---
+++ NLP for ML with Python - NLP Using Python and NLTK/it_mlnlpmdj_01_enus_13_samples/.ipynb_checkpoints/Exercise1script-checkpoint.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 00:43:49 2019
+
+@author: Welcome
+"""
+
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+
+sentence = "This is exercise that helps to understand what we have learnt"
+
+stopwords = set(stopwords.words('english')) 
+
+wordtokens = word_tokenize(sentence) 
+
+filtered = [w for w in wordtokens if not w in stopwords] 
+
+filtered = [] 
+
+for w in wordtokens: 
+	if w not in stopwords: 
+		filtered.append(w) 
+
+print(wordtokens) 
+print(filtered) 
